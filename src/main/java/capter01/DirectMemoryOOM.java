c31d90b9c8ebb76848babc9e8cd24a17c82e587a
xuchuanliang <xuchuanliang361@163.com>
2019-09-10 20:01:44 +0800

    commit

---
+++ src/main/java/capter01/DirectMemoryOOM.java
@@ -0,0 +1,22 @@
+package capter01;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author xuchuanliangbt
+ * @title: DirectMemoryOOM
+ * @projectName UnderstandingTheJVM
+ * @description:
+ * @date 2019/9/1019:57
+ * @Version
+ */
+public class DirectMemoryOOM {
+    private static final int _1MB = 1024*1024;
+    public static void main(String[] args) throws IllegalAccessException {
+        Field field = Unsafe.class.getDeclaredFields()[0];
+        field.setAccessible(true);
+        field.get(null);
+    }
+}
